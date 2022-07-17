2a49246c2f28f032b1af4573112d52a17eeecdc7
dbyr <david.michael.byrne17@gmail.com>
2022-07-17 10:33:04 +1000

    src: implement missing functions

--- src/bm.py
+++ src/bm.py
@@ -3,9 +3,12 @@ from typing import Dict
 
 def make_km_table(pattern: str) -> Dict[str, int]:
     table = dict()
-    raise Exception("TODO")
+    offset = len(pattern) - 1
+    # don't add the last character of the pattern
+    # because if the end char matches it won't progress
+    for i in range(offset):
+        table[pattern[i]] = len(pattern) - i - 1
     return table
-
 
 class Bm(object):
     def __init__(self, text: str, pattern: str):
@@ -15,9 +18,18 @@ class Bm(object):
 
     def decide_slide_width(self, c: str) -> int:
         assert len(c) == 1
-        raise Exception("TODO")
-        return -1
+        if c in self.table:
+            return self.table[c]
+        return len(self.pattern)
 
     def search(self) -> int:
-        raise Exception("TODO")
+        patlen = len(self.pattern)
+        i = patlen - 1
+        while i < len(self.text):
+            for j in range(patlen):
+                if self.text[i - j] != self.pattern[patlen - j - 1]:
+                    i += self.decide_slide_width(self.text[i - j])
+                    break
+            else:
+                return i - (patlen - 1)
         return -1
